3ac79c46e780cdea02faed7cbd4c396688c03c47
SURYANSH NAUGRAIYA <suryansh.naugraiya_cs18@gla.ac.in>
2020-01-12 00:37:07 +0530

    initial commit

--- src/List_Program4.py
+++ src/List_Program4.py
@@ -1,6 +1,10 @@
-# to create a list of first N prime numbers
-s = int(input("enter size of list"))
+# to print a list of N  prime numbers
+N = int(input("enter size"))
 l = []
-for i in range(0, s):
-    e = int(input("enter element"))
-    l.append(e)
+for i in range(2, 1000 * N):
+    for j in range(2, i + 1):
+        if (i % j) != 0:
+            l.append(i)
+    if l.index(i) == (N - 1):
+        break;
+print(l)
---
+++ src/List_Program5.py
@@ -0,0 +1,1 @@
+# to create a list of squares of first N natural numbers
