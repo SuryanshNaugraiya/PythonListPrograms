000844f3bce926fd2135f3e537f9dd6715713e81
SURYANSH NAUGRAIYA <suryansh.naugraiya_cs18@gla.ac.in>
2020-01-12 01:04:19 +0530

    printing the index of all occurence of element in a list

--- src/List_Program6.py
+++ src/List_Program6.py
@@ -1,3 +1,7 @@
 # to print the index of all occurence of a given element in a list
-l = [2, 3, 4, 5, 3, 1, 2, 4, 5]
-e = 2
+l = [2, 3, 4, 0, 3, 1, 2, 4, 3, 0]
+e = 0
+for i in l:
+    if i == e:
+        print(l.index(i))
+        l[l.index(i)] = ''
