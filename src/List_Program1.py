45bbb2f02e23b81072e8450dc0b6d6b4aa4f1bd3
SURYANSH NAUGRAIYA <suryansh.naugraiya_cs18@gla.ac.in>
2020-01-11 22:29:16 +0530

    appending elements in list

--- src/List_Program1.py
+++ src/List_Program1.py
@@ -1,3 +1,6 @@
 # program to sort a list given by user
 l = []
 s = eval(input("enter a list size"))
+for i in range(0, s):
+    element = input("enter element")
+    l.append(element)
--- src/List_Program2.py
+++ src/List_Program2.py
@@ -1,5 +1,3 @@
-for i in range(0, s):
-    element = input("enter element")
-    l.append(element)
+
 l.sort()
 print(l)
