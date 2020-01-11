d96dae4243e42c9b7d5be33d84f4bceede99da72
SURYANSH NAUGRAIYA <suryansh.naugraiya_cs18@gla.ac.in>
2020-01-12 01:47:20 +0530

    apllying for loop and if condition to count the frequency of element and appending it

--- src/List_Program8.py
+++ src/List_Program8.py
@@ -1,3 +1,7 @@
 # to print distinct list elements along with their frequency of occurence in the list
 l1 = []
 l2 = [2, 3, 4, 5, 1, 2, 3, 4, 2, 1, 5, 6, 5]
+for i in l2:
+    if i not in l1:
+        print(i, l2.count(i))
+        l1.append(i)
