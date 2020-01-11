615b94aaf747f97cfce43c707969ba1e5e57f228
SURYANSH NAUGRAIYA <suryansh.naugraiya_cs18@gla.ac.in>
2020-01-11 23:11:01 +0530

    printing greatest element

--- src/List_Proram2.py
+++ src/List_Proram2.py
@@ -1,7 +1,8 @@
-# to find ther greatest integer in list
+# to find the greatest integer in list
 s = int(input("enter a size of list"))
 l = []
-for i in (0, s):
-    e = int(input("emter element"))
+for i in range(0, s):
+    e = int(input("enter element"))
     l.append(e)
 l.sort()
+print(l[s - 1])
