eb5726ce7ca9901418bd536fb553b618b8061842
SURYANSH NAUGRAIYA <suryansh.naugraiya_cs18@gla.ac.in>
2020-01-12 01:19:43 +0530

    initialising s1 and s2 =0

--- src/List_Program7.py
+++ src/List_Program7.py
@@ -1,2 +1,4 @@
-# to print hte sum of all even no. and sum of all odd no. in the list
+# to print the sum of all even no. and sum of all odd no. in the list
 l = [2, 3, 4, 1, 2, 3, 7, 6, 9]
+s1 = 0
+s2 = 0
